408d06ed87c22904a872fc2a78af5c5447f97d86
rosell-dk <it@rosell.dk>
2018-08-18 21:26:36 +0200

    cleaned up a bit

--- webp-on-demand.php
+++ webp-on-demand.php
@@ -1,35 +1,14 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
 //require 'webp-on-demand/vendor/autoload.php';
-
-require 'vendor/webp-on-demand/autoload.php';
-//require 'vendor/webp-convert-and-serve/autoload.php';
 //require 'vendor/webp-on-demand/autoload.php';
-
-//echo '<br>done';
-
-//require '../webp-convert/WebPConvert.php';
-
-
-/*use WebPConvert\WebPConvert;
-
-$source = __DIR__ . '/test/test.jpg';
-$destination = __DIR__ . '/test/test.jpg.webp';
-
-// .. fire up WebP conversion
-$success = WebPConvert::convert($source, $destination, array(
-    'quality' => 90,
-    // more options available!
-));
-
-
-//$status = WebPOnDemand::serve(__DIR__);
-
-//echo 'hi';
-*/
-
+require 'vendor/require-webp-on-demand.php';
 
 use WebPOnDemand\WebPOnDemand;
+
 
 $status = WebPOnDemand::serve(__DIR__);
 if ($status < 0) {
