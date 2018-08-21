rosell-dk/webp-express

commit 045a4dd1894d5f50c32c5a1e12a95a02aea59af7

WebPExpress are now removed when plugin is uninstalled

webp-express.php
@@ -68,26 +68,35 @@ if (get_option('webp-express-deactivate')) {
     delete_option('webp-express-deactivate');
 }
 
-/*
-TODO:
-Delete options when plugin is uninstalled
+$optionsToDelete = [
+    'webp_express_max_quality',
+    'webp_express_image_types_to_convert',
+    'webp_express_failure_response',
+    'webp_express_converters',
+    'webp-express-inserted-rules-ok'
+];
+foreach ($optionsToDelete as $i => $optionName) {
+    delete_option($optionName);
+}
 
-But should it be registered inside the register_activation_hook, like in the user contributed example here? :
-https://developer.wordpress.org/reference/functions/register_uninstall_hook/
-
-
-function () {
-    delete_option('webp_express_max_quality');
-    delete_option('webp_express_image_types_to_convert');
-    delete_option('webp_express_failure_response');
-    delete_option('webp_express_converters');
-
+function webp_express_register_uninstall_hook() {
+    $optionsToDelete = [
+        'webp_express_max_quality',
+        'webp_express_image_types_to_convert',
+        'webp_express_failure_response',
+        'webp_express_converters',
+        'webp-express-inserted-rules-ok'
+    ];
+    foreach ($optionsToDelete as $i => $optionName) {
+        delete_option($optionName);
+    }
     /*
     webp_express_fail_action
     webp_express_method
     webp_express_quality
+    */
 }
-*/
+register_uninstall_hook( __FILE__, 'webp_express_register_uninstall_hook');
 
 
 // Add settings link on the plugins page

commit c5484d0e6c6ba3982631c7d2aa48e2aed4d7229f

whoopsie, big bug introduced with 045a4dd. All webp express options was constantly deleted! Related to #26

webp-express.php
@@ -68,17 +68,6 @@ if (get_option('webp-express-deactivate')) {
     delete_option('webp-express-deactivate');
 }
 
-$optionsToDelete = [
-    'webp_express_max_quality',
-    'webp_express_image_types_to_convert',
-    'webp_express_failure_response',
-    'webp_express_converters',
-    'webp-express-inserted-rules-ok'
-];
-foreach ($optionsToDelete as $i => $optionName) {
-    delete_option($optionName);
-}
-
 function webp_express_register_uninstall_hook() {
     $optionsToDelete = [
         'webp_express_max_quality',