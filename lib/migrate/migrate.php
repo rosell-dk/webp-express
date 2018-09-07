58be81d9ed4796c45b8ae5b698c68c1ff55efbb4
rosell-dk <it@rosell.dk>
2018-09-07 18:33:35 +0200

    more migration

--- lib/migrate/migrate.php
+++ lib/migrate/migrate.php
@@ -1,20 +1,42 @@
 <?php
 
-$currentMigration = intval(get_option('webp-express-migration-version', 0));
-
-if ($currentMigration == 0) {
-    // run migration 1
-    // It must take care of updating migration-version, - if successful.
-    include __DIR__ . '/migrate1.php';
-}
-
-// When a new version needs a new migration, uncomment this:
 /*
-if ($currentMigration == 1) {
-    // run migration 2
-    include __DIR__ . '/migrate2.php';
-}
+Unfortunately, we did not set the 'webp-express-configured' option back in 0.1
+As long as there are still users on 0.1, we must do the following:
 */
+// TODO: TEST the following
+if (!get_option('webp-express-configured', false)) {
+    if (!is_null(get_option('webp_express_converters', null))) {
+        update_option('webp-express-configured', true);
+    }
+}
+
+
+
+// If options never has been saved, no migration is needed.
+// - and we can then set migrate-version to current
+if (!(get_option('webp-express-configured', false))) {
+    update_option('webp-express-migration-version', WEBPEXPRESS_MIGRATION_VERSION);
+} else {
+
+    if (intval(get_option('webp-express-migration-version', 0)) == 0) {
+        // run migration 1
+        // It must take care of updating migration-version, - if successful.
+        include __DIR__ . '/migrate1.php';
+    }
+
+    // When a new version needs a new migration, uncomment this:
+    // (make sure to grab the option again - it might have been changed in the migration above)
+    /*
+    if (intval(get_option('webp-express-migration-version', 0)) == 1) {
+        // run migration 2
+        include __DIR__ . '/migrate2.php';
+    }
+    */
+
+}
+
+
 
 
 
