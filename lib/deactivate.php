8797e802d1810e95439a148b13a51b1a0e07e70d
rosell-dk <it@rosell.dk>
2018-09-10 14:47:35 +0200

    Added deactivation prevention, in case the rules in the .htaccess cannot be removed

--- lib/deactivate.php
+++ lib/deactivate.php
@@ -1,25 +1,71 @@
 <?php
 
+include_once __DIR__ . '/classes/Actions.php';
+use \WebPExpress\Actions;
+
 include_once __DIR__ . '/classes/Config.php';
 use \WebPExpress\Config;
 
 include_once __DIR__ . '/classes/Messenger.php';
 use \WebPExpress\Messenger;
 
+include_once __DIR__ . '/classes/Paths.php';
+use \WebPExpress\Paths;
 
-if (!Config::deactivateHTAccessRules()) {
 
-    /*
-    TODO!
-    if (Config::doesHTAccessExists()) {
-        Messenger::addMessage('error',
-            'Could not remove rewrite rules in the <i>.htaccess</i>. YOU wil have to do that! Now! (I recommend you reactivate WebP Express until you have solved the issue)'
-        );
-    } else {
-        Messenger::addMessage('warning',
-            'Plugin has been deactivated. Remember to remove the Rewrite Rules in your VirtualHost!'
-        );
+/**
+ *  Get .htaccess content (if possible)
+ */
+function webpexpress_get_htaccess_content() {
+    $htaccessFilename = Paths::getHTAccessFilename();
+    $handle = @fopen($htaccessFilename, "r");
+    if ($handle === false) {
+        return false;
     }
-    */
+    $content = @fread($handle, filesize($htaccessFilename));
+    if ($json === false) {
+        return false;
+    }
+    fclose($handle);
+    return $content;
+}
 
+function webpexpress_deny_deactivate() {
+    Messenger::addMessage(
+        'error',
+        "<b>Sorry, can't let you disable WebP Express!</b><br>" .
+            'There are rewrite rules in the <i>.htaccess</i> that could not be removed. If these are not removed, it would break all images.<br>' .
+            'Please make your <i>.htaccess</i> writable and then try to disable WebPExpress again.<br>Alternatively, remove the rules manually in your <i>.htaccess</i> file and try disabling again.'
+    );
+    wp_redirect( $_SERVER['HTTP_REFERER']);
+    exit;
+}
+
+if (Config::doesHTAccessExists()) {
+
+    // Try to deactivate the rules
+    if (!Config::deactivateHTAccessRules()) {
+
+        // Oh no. We failed removing the rules
+
+        // Sneak-peak into the .htaccess, to determine if we have rules there.
+        // (We may not be allowed)
+        $content = webpexpress_get_htaccess_content();
+        if ($content !== false) {
+            if (strpos($content, '# Redirect images to webp-on-demand.php') !== false) {
+                webpexpress_deny_deactivate();
+            }
+        } else {
+            // We were not allowed to sneak-peak
+            // We have other ways...
+            if (State::getState('htaccess-rules-saved-at-some-point', false)) {
+                $config = Config::loadConfig();
+                if ($config !== false) {
+                    if ($config['image-types'] > 0) {
+                        webpexpress_deny_deactivate();
+                    }
+                }
+            }
+        }
+    }
 }
