599c692d57bed1a453f6372fcb0f11dfba98af76
rosell-dk <it@rosell.dk>
2018-09-10 12:33:00 +0200

    now deletes old images too

--- lib/migrate/migrate1.php
+++ lib/migrate/migrate1.php
@@ -5,6 +5,11 @@ namespace WebPExpress;
 include_once __DIR__ . '/../classes/Config.php';
 use \WebPExpress\Config;
 
+/*
+include_once __DIR__ . '/../classes/Paths.php';
+use \WebPExpress\Paths;
+*/
+
 include_once __DIR__ . '/../classes/Messenger.php';
 use \WebPExpress\Messenger;
 
@@ -145,76 +150,35 @@ function webpexpress_migrate1_deleteOldOptions() {
         delete_option($optionName);
     }
 }
+
+/* helper. Remove dir recursively. No warnings - fails silently */
+function webpexpress_migrate1_rrmdir($dir) {
+   if (@is_dir($dir)) {
+     $objects = @scandir($dir);
+     foreach ($objects as $object) {
+       if ($object != "." && $object != "..") {
+         if (@is_dir($dir."/".$object))
+           webpexpress_migrate1_rrmdir($dir."/".$object);
+         else
+           @unlink($dir."/".$object);
+       }
+     }
+     @rmdir($dir);
+   }
+}
+
+function webpexpress_migrate1_deleteOldWebPImages() {
+    $upload_dir = wp_upload_dir();
+    $destinationRoot = trailingslashit($upload_dir['basedir']) . 'webp-express';
+    webpexpress_migrate1_rrmdir($destinationRoot);
+}
+
 if (webp_express_migrate1_createFolders()) {
     if (webp_express_migrate1_createDummyConfigFiles()) {
         if (webpexpress_migrate1_migrateOptions()) {
             webpexpress_migrate1_deleteOldOptions();
+            webpexpress_migrate1_deleteOldWebPImages();
             update_option('webp-express-migration-version', '1');
         }
     }
 }
-
-
-//echo 'migrate 05';
-/*
-$optionsToDelete = [
-    'webp_express_max_quality',
-    'webp_express_image_types_to_convert',
-    'webp_express_failure_response',
-    'webp_express_converters',
-    'webp-express-inserted-rules-ok',
-    'webp-express-configured',
-    'webp-express-pending-messages',
-    'webp-express-just-activated',
-    'webp-express-message-pending',
-    'webp-express-failed-inserting-rules',
-    'webp-express-deactivate'
-];
-foreach ($optionsToDelete as $i => $optionName) {
-    delete_option($optionName);
-}
-update_option('webp-express-version', '0.5', true);
-*/
-/*
-$converters_including_deactivated = json_decode(get_option('webp_express_converters', []), true);
-$converters = [];
-foreach ($converters_including_deactivated as $converter) {
-    if (isset($converter['deactivated'])) continue;
-
-    // Search for options containing "-2".
-    // If they exists, we must duplicate the converter.
-    $shouldDuplicate = false;
-    if (isset($converter['options'])) {
-        foreach ($converter['options'] as $converterOption => $converterValue) {
-            if (substr($converterOption, -2) == '-2') {
-                $shouldDuplicate = true;
-            }
-        }
-    };
-
-    if ($shouldDuplicate) {
-        // Duplicate converter
-        $converter2 = $converter;
-        foreach ($converter['options'] as $converterOption => $converterValue) {
-            if (substr($converterOption, -2) == '-2') {
-                unset ($converter['options'][$converterOption]);
-                unset ($converter2['options'][$converterOption]);
-                $one = substr($converterOption, 0, -2);
-                $converter2['options'][$one] = $converterValue;
-                //$converter[$converterOption] = null;
-            }
-        }
-        $converters[] = $converter;
-        $converters[] = $converter2;
-    } else {
-        $converters[] = $converter;
-    }
-}
-*/
-/*
-class Migrate05
-{
-
-    public static
-}
-*/
