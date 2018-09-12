adb51c69252999db67219dd97cbf4f2fc9b7e673
rosell-dk <it@rosell.dk>
2018-09-12 15:33:56 +0200

    cleanup

--- lib/deactivate.php
+++ lib/deactivate.php
@@ -30,10 +30,6 @@ if (Config::doesHTAccessExists()) {
 
         // Oh no. We failed removing the rules
 
-        // Sneak-peak into the .htaccess, to determine if we have rules there.
-        // (We may not be allowed)
-//deactivateHTAccessRules
-
         $result = Config::deactivateHTAccessRules();
         if ($result !== true) {
             $msg = "<b>Sorry, can't let you disable WebP Express!</b><br>" .
@@ -41,24 +37,6 @@ if (Config::doesHTAccessExists()) {
                 'Please make your <i>.htaccess</i> writable and then try to disable WebPExpress again.<br>Alternatively, remove the rules manually in your <i>.htaccess</i> file and try disabling again.' .
                 '<br>It conserns the following files:' . implode('<br>', $result);
             webpexpress_deny_deactivate($msg);
-        }
-
-        $content = webpexpress_get_htaccess_content();
-        if ($content !== false) {
-            if (strpos($content, '# Redirect images to webp-on-demand.php') !== false) {
-                webpexpress_deny_deactivate();
-            }
-        } else {
-            // We were not allowed to sneak-peak
-            // We have other ways...
-            if (State::getState('htaccess-rules-saved-at-some-point', false)) {
-                $config = Config::loadConfig();
-                if ($config !== false) {
-                    if ($config['image-types'] > 0) {
-                        webpexpress_deny_deactivate();
-                    }
-                }
-            }
         }
     }
 }
