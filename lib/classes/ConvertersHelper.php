bf6f8bed4d90a78e3a0375f57571c2b85e4a5a98
rosell-dk <it@rosell.dk>
2018-10-04 11:21:15 +0200

    Found myself working a lot on these converter arrays...

---
+++ lib/classes/ConvertersHelper.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace WebPExpress;
+
+class ConvertersHelper
+{
+    public static $defaultConverters = [
+        ['converter' => 'gd', 'options' => ['skip-pngs' => true]],
+        ['converter' => 'cwebp', 'options' => [
+            'use-nice' => false,
+            'try-common-system-paths' => true,
+            'try-supplied-binary-for-os' => true,
+            'method' => 6,
+            'size-in-percentage' => 45,
+            'low-memory' => false,
+            'command-line-options' => '-low_memory',
+        ]],
+        ['converter' => 'imagick'],
+        ['converter' => 'gmagick'],
+        ['converter' => 'wpc', 'options' => ['quality' => 'auto']],
+        ['converter' => 'ewww'],
+    ];
+
+    public static function getDefaultConverterNames()
+    {
+        return array_column(self::$defaultConverters, 'converter');
+    }
+
+    public static function getConverterNames($converters)
+    {
+        return array_column(self::normalize($converters), 'converter');
+    }
+
+    public static function normalize($converters)
+    {
+        foreach ($converters as &$converter) {
+            if (!isset($converter['converter'])) {
+                $converter = ['converter' => $converter];
+            }
+            if (!isset($converter['options'])) {
+                $converter['options'] = [];
+            }
+        }
+        return $converters;
+    }
+
+    /**
+     *  Those converters in second, but not in first will be appended to first
+     */
+    public static function mergeConverters($first, $second)
+    {
+        $namesInFirst = self::getConverterNames($first);
+        $second = self::normalize($second);
+
+        foreach ($second as $converter) {
+            if (!in_array($converter['converter'], $namesInFirst)) {
+                $first[] = $converter;
+            }
+        }
+        return $first;
+    }
+
+}
