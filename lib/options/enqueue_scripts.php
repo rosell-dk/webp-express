be6aaf177f98a31257dc5d3238e819016022f694
rosell-dk <it@rosell.dk>
2018-10-11 12:57:44 +0200

    added whitelist and renamed the other js to "converters.js"

--- lib/options/enqueue_scripts.php
+++ lib/options/enqueue_scripts.php
@@ -6,17 +6,15 @@ use \WebPExpress\Paths;
 wp_register_script('sortable', plugins_url('js/sortable.min.js', __FILE__), [], '1.9.0');
 wp_enqueue_script('sortable');
 
-wp_register_script(
-    'webp-express-options-page',
-    plugins_url('js/webp-express-options-page.js', __FILE__),
-    ['sortable'],
-    '0.6.0-1'
-);
-wp_enqueue_script('webp-express-options-page');
+wp_register_script('converters', plugins_url('js/converters.js', __FILE__), ['sortable'], '0.7.0-dev3');
+wp_enqueue_script('converters');
+
+wp_register_script( 'whitelist', plugins_url('js/whitelist.js', __FILE__), [], '0.7.0-dev2');
+wp_enqueue_script('whitelist');
 
 if (function_exists('wp_add_inline_script')) {
     // wp_add_inline_script is available from Wordpress 4.5
-    wp_add_inline_script('webp-express-options-page', 'window.webpExpressPaths = ' . json_encode(Paths::getUrlsAndPathsForTheJavascript()) . ';');
+    wp_add_inline_script('converters', 'window.webpExpressPaths = ' . json_encode(Paths::getUrlsAndPathsForTheJavascript()) . ';');
 } else {
     echo '<script>window.webpExpressPaths = ' . json_encode(Paths::getUrlsAndPathsForTheJavascript()) . ';</script>';
 }
