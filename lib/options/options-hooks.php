d273790e2dcdc6573cf12a64d96df1fc04ba8979
rosell-dk <it@rosell.dk>
2018-12-27 09:33:48 +0100

    minor

--- lib/options/options-hooks.php
+++ lib/options/options-hooks.php
@@ -16,16 +16,20 @@ add_action('admin_post_webpexpress_settings_submit', function() {
     include __DIR__ . '/submit.php';
 });
 
-add_action( 'admin_init', function () {
+function webp_express_settings_page_content()
+{
+    //include __DIR__ . '/enqueue_scripts.php';     // we don't do that, because our inline scripts would not be written in head on wordpress 4.4 and below
+    include __DIR__ . '/page.php';
+}
+
+function webp_express_admin_init() {
+
 	global $pagenow;
 	if ( ( 'options-general.php' === $pagenow ) && ( isset( $_GET['page'] ) ) && ( 'webp_express_settings_page' === $_GET['page'] ) ) {
 		add_action( 'admin_enqueue_scripts', function () {
 			include __DIR__ . '/enqueue_scripts.php';
 		} );
 	}
-} );
-
-function webp_express_settings_page_content()
-{
-    include __DIR__ . '/page.php';
 }
+
+add_action( 'admin_init', 'webp_express_admin_init');
