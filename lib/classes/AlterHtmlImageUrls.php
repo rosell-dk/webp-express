6ffba4b210bca616fb2736b419e470e406f0cc7e
rosell-dk <it@rosell.dk>
2019-01-11 13:05:35 +0100

    minor

--- lib/classes/AlterHtmlImageUrls.php
+++ lib/classes/AlterHtmlImageUrls.php
@@ -26,41 +26,34 @@ class AlterHtmlImageUrls
         // We look for a "ref" attribute, but what is that used for?
 
         // TODO: also replace attributes typically used for lazy loading (see AlterHtmlPicture.php)
-        
+
         $regex_rule = '#(?<=(?:(ref|src|set)=[\"\']))(?:http[s]?[^\"\']+)(\.png|\.jp[e]?g)(?:[^\"\']+)?(?=[\"\')])#';
-        return preg_replace_callback($regex_rule, 'self::_convertWebP', $content);
+        return preg_replace_callback($regex_rule, 'self::replaceCallback', $content);
     }
 
-    private static function _convertWebP($asset) {
-        list($src, $attr) = $asset;
-        switch ($attr) {
+    private static function replaceCallback($match) {
+        list($attrValue, $attrName) = $match;
+        switch ($attrName) {
             case 'src':
             case 'ref':
-                return self::_convertWebPSrc($src);
+                return \WebPExpress\AlterHtmlHelper::getWebPUrl($attrValue, $attrValue);
+
             case 'set':
-                return self::_convertWebPSrcSet($src);
+                $srcsetArr = explode(',', $attrValue);
+                foreach ($srcsetArr as $i => $srcSetEntry) {
+                    // $srcSetEntry is ie "http://example.com/image.jpg 520w"
+                    list($src, $width) = preg_split('/\s+/', trim($srcSetEntry));        // $width might not be set, but thats ok.
+
+                    $webpUrl = \WebPExpress\AlterHtmlHelper::getWebPUrl($src, false);
+                    if ($webpUrl !== false) {
+                        $srcsetArr[$i] = $webpUrl . (isset($width) ? ' ' . $width : '');
+                    }
+                }
+                return implode(', ', $srcsetArr);
+
             default:
                 return $src;
         }
     }
 
-    private static function _convertWebPSrc($src) {
-        return \WebPExpress\AlterHtmlHelper::getWebPUrl($src, $src);
-    }
-
-    private static function _convertWebPSrcSet($srcset) {
-
-        $srcsetArr = explode(', ', $srcset);
-        foreach ($srcsetArr as $i => $srcSetEntry) {
-            // $srcSetEntry is ie "http://example.com/image.jpg 520w"
-            list($src, $width) = explode(' ', $srcSetEntry);
-
-            $webpUrl = \WebPExpress\AlterHtmlHelper::getWebPUrl($src, false);
-            if ($webpUrl !== false) {
-                $srcsetArr[$i] = $webpUrl . ' ' . $width;
-            }
-        }
-        $srcset = implode(', ', $srcsetArr);
-        return $srcset;
-    }
 }
