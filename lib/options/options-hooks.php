ca69a4039b43b64c806ae3e33ca4adbcbfcd9898
rosell-dk <it@rosell.dk>
2019-02-20 17:13:24 +0100

    Do not add options menu for network. #173

--- lib/options/options-hooks.php
+++ lib/options/options-hooks.php
@@ -1,17 +1,30 @@
 <?php
 use \WebPExpress\Config;
+use \WebPExpress\Multisite;
 
-add_action( 'admin_menu', function() {
-
-    //Add Settings Page
-    add_options_page(
-        'WebP Express Settings', //Page Title
-        'WebP Express', //Menu Title
-        'manage_options', //capability
-        'webp_express_settings_page', // slug
-        'webp_express_settings_page_content' //The function to be called to output the content for this page.
-    );
-});
+if (Multisite::isNetworkActivated()) {
+    add_action("network_admin_menu", function() {
+        add_submenu_page(
+    		'settings.php', // Parent element
+    		'WebP Express settings (for network)', // Text in browser title bar
+    		'WebP Express', // Text to be displayed in the menu.
+    		'manage_network_options', // Capability
+    		'webp_express_settings_page', // slug
+    		'webp_express_settings_page_content2' // Callback function which displays the page
+    	);
+    });
+} else {
+    add_action( 'admin_menu', function() {
+        //Add Settings Page
+        add_options_page(
+            'WebP Express Settings', //Page Title
+            'WebP Express', //Menu Title
+            'manage_options', //capability
+            'webp_express_settings_page', // slug
+            'webp_express_settings_page_content' //The function to be called to output the content for this page.
+        );
+    });
+}
 
 add_action('admin_post_webpexpress_settings_submit', function() {
     include __DIR__ . '/submit.php';
@@ -51,17 +64,4 @@ function webp_express_settings_page_content2()
     echo '<pre>' . print_r($config) . '</pre>';
 */
     include __DIR__ . '/page.php';
-}
-
-add_action("network_admin_menu", 'webpexpress_network_admin_menu');
-
-function webpexpress_network_admin_menu() {
-    add_submenu_page(
-		'settings.php', // Parent element
-		'WebP Express settings (for network)', // Text in browser title bar
-		'WebP Express', // Text to be displayed in the menu.
-		'manage_network_options', // Capability
-		'webp_express_settings_page', // slug
-		'webp_express_settings_page_content2' // Callback function which displays the page
-	);
 }
