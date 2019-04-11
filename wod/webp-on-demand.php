fc45c56eb96a749c3e0b4f4f8be2897510a16084
rosell-dk <it@rosell.dk>
2019-04-11 07:51:57 +0200

    Do not protect against direct access if nginx is in SERVER_SOFTWARE string (some have both nginx and apache)

--- wod/webp-on-demand.php
+++ wod/webp-on-demand.php
@@ -29,7 +29,7 @@ function exitWithError($msg) {
 
 // Protect against directly accessing webp-on-demand.php
 // Only protect on Apache. We know for sure that the method is not reliable on nginx. We have not tested on litespeed yet, so we dare not.
-if (stripos($_SERVER["SERVER_SOFTWARE"], 'apache') !== false) {
+if (stripos($_SERVER["SERVER_SOFTWARE"], 'apache') !== false && stripos($_SERVER["SERVER_SOFTWARE"], 'nginx') === false) {
     if (strpos($_SERVER['REQUEST_URI'], 'webp-on-demand.php') !== false) {
         exitWithError('It seems you are visiting this file (plugins/webp-express/wod/webp-on-demand.php) directly. We do not allow this.');
         exit;
