f67f9f8124817b629356baa4cc1e09af5efaaba0
rosell-dk <it@rosell.dk>
2019-05-27 13:52:00 +0200

    minor (textual)

--- lib/options/options/conversion-options/converter-options/gd.php
+++ lib/options/options/conversion-options/converter-options/gd.php
@@ -5,8 +5,15 @@
           <label for="gd_skip_pngs">Skip PNGs</label>
           <input type="checkbox" id="gd_skip_pngs">
           <br>
-          You can choose to skip PNG's for Gd (which means the next working and active converter in the stack will handle it, if there is any).
-          In our first implementation, Gd had problems with transparency. This is however solved now.          
+          <p>
+          You can choose to skip PNG's for Gd (which means the next working and active converter in the stack will handle it,
+          if there is any). There can be two reasons to do that:
+              <ul class="with-bullets">
+                  <li>In our first implementation, Gd had problems with transparency. This should however be solved now. </li>
+                  <li>Gd does not compress PNGs very effeciently</li>
+              </ul>
+          </p>
+
       </div>
       <br>
       <?php webp_express_printUpdateButtons() ?>
