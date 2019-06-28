8b7600d0398a8343eaa6d8d3cb6bcd2b98e474c7
rosell-dk <it@rosell.dk>
2019-06-28 22:59:02 +0200

    Added vendor folder to git. This makes it easier to download and use.

---
+++ vendor/rosell-dk/dom-util-for-webp/src/ImageUrlReplacer.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace DOMUtilForWebP;
+
+//use Sunra\PhpSimple\HtmlDomParser;
+
+/**
+ *  Highly configurable class for replacing image URLs in HTML (both src and srcset syntax)
+ *
+ *  Based on http://simplehtmldom.sourceforge.net/ - a library for easily manipulating HTML by means of a DOM.
+ *  The great thing about this library is that it supports working on invalid HTML and it only applies the changes you
+ *  make - very gently.
+ *
+ * TODO: Check out how ewww does it
+ *
+ *  Behaviour can be customized by overriding the public methods (replaceUrl, $searchInTags, etc)
+ *
+ *  Default behaviour:
+ *  - The modified URL is the same as the original, with ".webp" appended                   (replaceUrl)
+ *  - Limits to these tags: <img>, <source>, <input> and <iframe>                           ($searchInTags)
+ *  - Limits to these attributes: "src", "src-set" and any attribute starting with "data-"  (attributeFilter)
+ *  - Only replaces URLs that ends with "png", "jpg" or "jpeg" (no query strings either)    (replaceUrl)
+ *
+ *
+ */
+class ImageUrlReplacer
+{
+
+    // define tags to be searched. The div and li are on the list because these are often used with lazy loading
+    public static $searchInTags = ['img', 'source', 'input', 'iframe', 'div', 'li'];
+
+    /**
+     *
+     * @return webp url or, if URL should not be changed, return nothing
+     **/
+    public function replaceUrl($url)
+    {
+        if (!preg_match('#(png|jpe?g)$#', $url)) {
+            return;
+        }
+        return $url . '.webp';
+    }
+
+    public function replaceUrlOr($url, $returnValueIfDenied)
+    {
+        $url = $this->replaceUrl($url);
+        return (isset($url) ? $url : $returnValueIfDenied);
+    }
+
+    /*
+    public function isValidUrl($url)
+    {
+        return preg_match('#(png|jpe?g)$#', $url);
+    }*/
+
+    public function handleSrc($attrValue)
+    {
+        return $this->replaceUrlOr($attrValue, $attrValue);
+    }
+
+    public function handleSrcSet($attrValue)
+    {
+        // $attrValue is ie: <img data-x="1.jpg 1000w, 2.jpg">
+        $srcsetArr = explode(',', $attrValue);
+        foreach ($srcsetArr as $i => $srcSetEntry) {
+            // $srcSetEntry is ie "image.jpg 520w", but can also lack width, ie just "image.jpg"
+            // it can also be ie "image.jpg 2x"
+            $srcSetEntry = trim($srcSetEntry);
+            $entryParts = preg_split('/\s+/', $srcSetEntry, 2);
+            if (count($entryParts) == 2) {
+                list($src, $descriptors) = $entryParts;
+            } else {
+                $src = $srcSetEntry;
+                $descriptors = null;
+            }
+
+            $webpUrl = $this->replaceUrlOr($src, false);
+            if ($webpUrl !== false) {
+                $srcsetArr[$i] = $webpUrl . (isset($descriptors) ? ' ' . $descriptors : '');
+            }
+        }
+        return implode(', ', $srcsetArr);
+    }
+
+    /**
+     *  Test if attribute value looks like it has srcset syntax.
+     *  "image.jpg 100w" does for example. And "image.jpg 1x". Also "image1.jpg, image2.jpg 1x"
+     *  Mixing x and w is invalid (according to
+     *         https://stackoverflow.com/questions/26928828/html5-srcset-mixing-x-and-w-syntax)
+     *  But we accept it anyway
+     *  It is not the job of this function to see if the first part is an image URL
+     *  That will be done in handleSrcSet.
+     *
+     */
+    public function looksLikeSrcSet($value)
+    {
+        if (preg_match('#\s\d*(w|x)#', $value)) {
+            return true;
+        }
+        return false;
+    }
+
+    public function handleAttribute($value)
+    {
+        if (self::looksLikeSrcSet($value)) {
+            return self::handleSrcSet($value);
+        }
+        return self::handleSrc($value);
+    }
+
+    public function attributeFilter($attrName)
+    {
+        $attrName = strtolower($attrName);
+        if (($attrName == 'src') || ($attrName == 'srcset') || (strpos($attrName, 'data-') === 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    public function processCSSRegExCallback($matches)
+    {
+        list($all, $pre, $quote, $url, $post) = $matches;
+        return $pre . $this->replaceUrlOr($url, $url) . $post;
+    }
+
+    public function processCSS($css)
+    {
+        $declarations = explode(';', $css);
+        foreach ($declarations as $i => &$declaration) {
+            if (preg_match('#(background(-image)?)\\s*:#', $declaration)) {
+                // https://regexr.com/46qdg
+                //$regex = '#(url\s*\(([\"\']?))([^\'\";\)]*)(\2\s*\))#';
+                $parts = explode(',', $declaration);
+                //print_r($parts);
+                foreach ($parts as &$part) {
+                    //echo 'part:' . $part . "\n";
+                    $regex = '#(url\\s*\\(([\\"\\\']?))([^\\\'\\";\\)]*)(\\2\\s*\\))#';
+                    $part = preg_replace_callback($regex, 'self::processCSSRegExCallback', $part);
+                    //echo 'result:' . $part . "\n";
+                }
+                $declarations[$i] = implode($parts, ',');
+            }
+        }
+        return implode(';', $declarations);
+    }
+
+    public function replaceHtml($html)
+    {
+        if ($html == '') {
+            return '';
+        }
+
+        // https://stackoverflow.com/questions/4812691/preserve-line-breaks-simple-html-dom-parser
+
+        // function str_get_html($str, $lowercase=true, $forceTagsClosed=true, $target_charset = DEFAULT_TARGET_CHARSET,
+        //    $stripRN=true, $defaultBRText=DEFAULT_BR_TEXT, $defaultSpanText=DEFAULT_SPAN_TEXT)
+
+        //$dom = HtmlDomParser::str_get_html($html, false, false, 'UTF-8', false);
+        $dom = str_get_html($html, false, false, 'UTF-8', false);
+        if ($dom === false) {
+            if (strlen($html) > MAX_FILE_SIZE) {
+                return '<!-- Alter HTML was skipped because the HTML is too big to process! ' .
+                    '(limit is set to ' . MAX_FILE_SIZE . ' bytes) -->' . "\n" . $html;
+            }
+            return '<!-- Alter HTML was skipped because the helper library refused to process the html -->' .
+                "\n" . $html;
+        }
+
+        // Replace attributes (src, srcset, data-src, etc)
+        foreach (self::$searchInTags as $tagName) {
+            $elems = $dom->find($tagName);
+            foreach ($elems as $index => $elem) {
+                $attributes = $elem->getAllAttributes();
+                foreach ($elem->getAllAttributes() as $attrName => $attrValue) {
+                    if ($this->attributeFilter($attrName)) {
+                        $elem->setAttribute($attrName, $this->handleAttribute($attrValue));
+                    }
+                }
+            }
+        }
+
+        // Replace <style> elements
+        $elems = $dom->find('style');
+        foreach ($elems as $index => $elem) {
+            $css = $this->processCSS($elem->innertext);
+            if ($css != $elem->innertext) {
+                $elem->innertext = $css;
+            }
+        }
+
+        // Replace "style attributes
+        $elems = $dom->find('*[style]');
+        foreach ($elems as $index => $elem) {
+            $css = $this->processCSS($elem->style);
+            if ($css != $elem->style) {
+                $elem->style = $css;
+            }
+        }
+
+        return $dom->save();
+    }
+
+    /* Main replacer function */
+    public static function replace($html)
+    {
+        if (!function_exists('str_get_html')) {
+            require_once 'simple_html_dom/simple_html_dom.inc';
+        }
+        $iur = new static();
+        return $iur->replaceHtml($html);
+    }
+}
---
+++ vendor/rosell-dk/webp-convert-cloud-service/src/Serve.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace WebPConvertCloudService;
+
+use \WebPConvertCloudService\WebPConvertCloudService;
+use \WebPConvert\WebPConvert;
+
+class Serve
+{
+
+    private static function configurationError($msg)
+    {
+        WebPConvertCloudService::exitWithError(WebPConvertCloudService::ERROR_CONFIGURATION, $msg);
+    }
+
+    private static function accessDenied($msg)
+    {
+        WebPConvertCloudService::exitWithError(WebPConvertCloudService::ERROR_ACCESS_DENIED, $msg);
+    }
+
+    private static function runtimeError($msg)
+    {
+        WebPConvertCloudService::exitWithError(WebPConvertCloudService::ERROR_RUNTIME, $msg);
+    }
+
+    public static function serve($options)
+    {
+        $uploaddir = $options['destination-dir'] ;
+
+        if (!is_dir($uploaddir)) {
+            if (!@mkdir($uploaddir, 0775, true)) {
+                self::configurationError('Could not create folder for converted files: ' . $uploaddir);
+            }
+            @chmod($uploaddir, 0775);
+        }
+
+        /*
+        if (!isset($_POST['hash'])) {
+            self::accessDenied('Restricted access. Hash required, but missing');
+        }*/
+
+        if (!isset($_FILES['file'])) {
+            self::runtimeError('No file was supplied');
+        }
+        if (!isset($_FILES['file']['error'])) {
+            self::runtimeError('Invalid parameters');
+        }
+
+        if (is_array($_FILES['file']['error'])) {
+            self::runtimeError('Cannot convert multiple files');
+        }
+
+        switch ($_FILES['file']['error']) {
+            case UPLOAD_ERR_OK:
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                self::runtimeError('No file sent');
+                break;
+            case UPLOAD_ERR_INI_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+                self::runtimeError('Exceeded filesize limit.');
+                break;
+            default:
+                self::runtimeError('Unknown error.');
+        }
+
+        if ($_FILES['file']['size'] == 0) {
+            self::accessDenied('File size is zero. Perhaps exceeded filesize limit?');
+        }
+        // Undefined | Multiple Files | $_FILES Corruption Attack
+            // If this request falls under any of them, treat it invalid.
+        /*if ($_FILES['file']['size'] > 1000000) {
+            throw new RuntimeException('Exceeded filesize limit.');
+        }*/
+
+        // DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
+        // Check MIME Type by yourself.
+        if (function_exists('finfo_file') && (defined('FILEINFO_MIME_TYPE'))) {
+            $r = finfo_open(FILEINFO_MIME_TYPE);
+            if (false === $ext = array_search(
+                finfo_file($r, $_FILES['file']['tmp_name']),
+                array(
+                    'jpg' => 'image/jpeg',
+                    'png' => 'image/png',
+                    'gif' => 'image/gif',
+                ),
+                true
+            )) {
+                self::accessDenied('Invalid file format.');
+            }
+        } else {
+            $ext = 'jpg';   // We set it to something, in case above fails.
+        }
+
+        $uploadfile = $uploaddir . '/' . sha1_file($_FILES['file']['tmp_name']) . '.' . $ext;
+        //echo $uploadfile;
+        if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
+            // File is valid, and was successfully uploaded
+
+            $source = $uploadfile;
+
+            /*
+            if (!empty($password)) {
+                $hash = md5(md5_file($source) . $password);
+
+                if ($hash != $_POST['hash']) {
+                    self::accessDenied('Wrong password.');
+                }
+            }
+            */
+            $destination = $uploadfile . '.webp';
+
+            if (isset($_POST['options'])) {
+                // Merge in options in $_POST, overwriting the webp-convert options in config
+                $convertOptionsInPost = (array) json_decode($_POST['options']);
+                $convertOptions = array_merge($options['webp-convert'], $convertOptionsInPost);
+            } else {
+                $convertOptions = $options['webp-convert'];
+            }
+
+            try {
+                WebPConvert::convert($source, $destination, $convertOptions);
+                header('Content-type: application/octet-stream');
+                echo file_get_contents($destination);
+
+                unlink($source);
+                unlink($destination);
+            } catch (\Exception $e) {
+                echo 'Conversion failed!';
+                echo $e->getMessage();
+            }
+        } else {
+            // Possible file upload attack!
+            self::configurationError('Failed to move uploaded file');
+
+            //echo 'Failed to move uploaded file';
+        }
+    }
+}
