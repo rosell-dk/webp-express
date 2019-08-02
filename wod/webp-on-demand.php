835d6f3d85db995f3abef1c0f86f5ca79a1b2540
rosell-dk <it@rosell.dk>
2019-08-02 05:56:22 +0200

    Fixed typing error in class name

--- wod/webp-on-demand.php
+++ wod/webp-on-demand.php
@@ -14,7 +14,7 @@ use \WebPExpress\SanityException;
 use \WebPExpress\ValidateException;
 use \WebPExpress\Validate;
 
-class WebPOnDempand
+class WebPOnDemand
 {
 
     private static $docRoot;
@@ -223,9 +223,11 @@ class WebPOnDempand
 // Only protect on Apache. We know for sure that the method is not reliable on nginx. We have not tested on litespeed yet, so we dare not.
 if (stripos($_SERVER["SERVER_SOFTWARE"], 'apache') !== false && stripos($_SERVER["SERVER_SOFTWARE"], 'nginx') === false) {
     if (strpos($_SERVER['REQUEST_URI'], 'webp-on-demand.php') !== false) {
-        WebPOnDempand::exitWithError('It seems you are visiting this file (plugins/webp-express/wod/webp-on-demand.php) directly. We do not allow this.');
+        WebPOnDemand::exitWithError(
+            'It seems you are visiting this file (plugins/webp-express/wod/webp-on-demand.php) directly. We do not allow this.'
+        );
         exit;
     }
 }
 
-WebPOnDempand::process();
+WebPOnDemand::process();
