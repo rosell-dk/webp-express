d6aba98fa1acbfe32c906441011814326541eb88
rosell-dk <it@rosell.dk>
2019-08-02 05:56:43 +0200

    minor (cs)

--- wod/webp-realizer.php
+++ wod/webp-realizer.php
@@ -210,7 +210,9 @@ class WebPRealizer
 // Only protect on Apache. We know for sure that the method is not reliable on nginx. We have not tested on litespeed yet, so we dare not.
 if (stripos($_SERVER["SERVER_SOFTWARE"], 'apache') !== false && stripos($_SERVER["SERVER_SOFTWARE"], 'nginx') === false) {
     if (strpos($_SERVER['REQUEST_URI'], 'webp-realizer.php') !== false) {
-        WebPRealizer::exitWithError('It seems you are visiting this file (plugins/webp-express/wod/webp-realizer.php) directly. We do not allow this.');
+        WebPRealizer::exitWithError(
+            'It seems you are visiting this file (plugins/webp-express/wod/webp-realizer.php) directly. We do not allow this.'
+        );
         exit;
     }
 }
