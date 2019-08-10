787a905ae25dc4bef3fc87e13212f31000e8464e
rosell-dk <it@rosell.dk>
2019-08-10 22:04:23 +0200

    0.14.22

--- lib/classes/ConvertHelperIndependent.php
+++ lib/classes/ConvertHelperIndependent.php
@@ -326,7 +326,7 @@ APACHE
 
         $text = preg_replace('#' . preg_quote($_SERVER["DOCUMENT_ROOT"]) . '#', '[doc-root]', $text);
 
-        $text = 'WebP Express 0.14.21. ' . $msgTop . ', ' . date("Y-m-d H:i:s") . "\n\r\n\r" . $text;
+        $text = 'WebP Express 0.14.22. ' . $msgTop . ', ' . date("Y-m-d H:i:s") . "\n\r\n\r" . $text;
 
         $logFile = self::getLogFilename($source, $logDir);
 
