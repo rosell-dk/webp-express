21245f02407d32eb84963a07359461460cc344c2
rosell-dk <it@rosell.dk>
2019-09-17 14:38:47 +0200

    minor textual

--- lib/dismissable-messages/0.15.1/problems-with-mingled-set.php
+++ lib/dismissable-messages/0.15.1/problems-with-mingled-set.php
@@ -4,7 +4,8 @@ namespace WebPExpress;
 
 DismissableMessages::printDismissableMessage(
     'error',
-    'Sorry, due to a bug, the combination of destination folder: mingled and destination extension: set ' .
+    'Sorry, due to a bug, the combination of having destination folder set to "mingled" and ' .
+        'File extension set to "Set to .webp" ' .
         'does not currently work. Please change the settings. ' .
         'I shall fix this soon!',
     '0.15.1/problems-with-mingled-set',
