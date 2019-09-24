950a96b02755038ee342af7cca07abc459f25c8f
rosell-dk <it@rosell.dk>
2019-09-24 21:11:15 +0200

    dir change

--- lib/options/enqueue_scripts.php
+++ lib/options/enqueue_scripts.php
@@ -6,7 +6,7 @@ use \WebPExpress\Paths;
 use \WebPExpress\Config;
 
 $ver = '1';             // note: Minimum 1
-$jsDir = 'js/0.15.0';   // We change dir when it is critical that no-one gets the cached version (there is a plugin that strips version strings out there...)
+$jsDir = 'js/0.16.0';   // We change dir when it is critical that no-one gets the cached version (there is a plugin that strips version strings out there...)
 
 if (!function_exists('webp_express_add_inline_script')) {
     function webp_express_add_inline_script($id, $script, $position) {
