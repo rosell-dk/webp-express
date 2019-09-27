4b16ce368d85cbd133e223a67d2f2a16aa24673e
rosell-dk <it@rosell.dk>
2019-09-27 12:38:51 +0200

    minor

--- lib/classes/ConvertHelperIndependent.php
+++ lib/classes/ConvertHelperIndependent.php
@@ -570,6 +570,7 @@ APACHE
 
         $text = preg_replace('#' . preg_quote($_SERVER["DOCUMENT_ROOT"]) . '#', '[doc-root]', $text);
 
+        // TODO: Put version number somewhere else. Ie \WebPExpress\VersionNumber::version
         $text = 'WebP Express 0.17.0. ' . $msgTop . ', ' . date("Y-m-d H:i:s") . "\n\r\n\r" . $text;
 
         $logFile = self::getLogFilename($source, $logDir);
