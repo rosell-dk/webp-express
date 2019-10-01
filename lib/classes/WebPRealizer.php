rosell-dk/webp-express

commit 2bce3044bdda01b808daedf3efc91ecead659c01

Tell nginx users if xdestination arg in Nginx rules is missing for webp-realizer.php. #365

lib/classes/WebPRealizer.php
@@ -52,8 +52,19 @@ class WebPRealizer extends WodConfigLoader
         }
 
         // Last resort is to use $_SERVER['REQUEST_URI'], well knowing that it does not give the
-        // correct result in all setups (ie "folder method 1")
+        // correct result in all setups (ie "folder method 1").
+        // On nginx, it can even return the path to webp-realizer.php. TODO: Handle that better than now
         $destRel = SanityCheck::pathWithoutDirectoryTraversal(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+        if ($destRel) {
+            if (preg_match('#webp-realizer\.php$#', $destRel)) {
+                throw new \Exception(
+                    'webp-realizer.php need to know the file path and cannot simply use $_SERVER["REQUEST_URI"] ' .
+                    'as that points to itself rather than the image requested. ' .
+                    'On Nginx, please add: "&xdestination=x$request_filename" to the URL in the rules in the nginx config ' .
+                    '(sorry, the parameter was missing in the rules in the README for a while, but it is back)'
+                );
+            }
+        }
         $destination = SanityCheck::absPath($docRoot . $destRel);
         return SanityCheck::absPathIsInDocRoot($destination);
     }

commit 4df921a4622d70fd478f57a77df193902acb1fdc

Fixed bug introduced in 2bce304 (was not released). #365

lib/classes/WebPRealizer.php
@@ -39,15 +39,13 @@ class WebPRealizer extends WodConfigLoader
 
         // Check querystring (full path)
         // - But only on Nginx (our Apache .htaccess rules never passes absolute url)
-        if (
-            (self::isNginxHandlingImages()) &&
-            (isset($_GET['destination']) || isset($_GET['xdestination']))
-        ) {
+        if (self::isNginxHandlingImages()) {
             if (isset($_GET['destination'])) {
-                $destination = SanityCheck::absPathIsInDocRoot($_GET['destination']);
-            } else {
+                return SanityCheck::absPathIsInDocRoot($_GET['destination']);
+            }
+            if (isset($_GET['xdestination'])) {
                 $xdest = SanityCheck::noControlChars($_GET['xdestination']);
-                $destination = SanityCheck::absPathIsInDocRoot(substr($xdest, 1));
+                return SanityCheck::absPathIsInDocRoot(substr($xdest, 1));
             }
         }
 