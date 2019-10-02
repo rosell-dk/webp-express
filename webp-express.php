7681472160279e1f4f48e5f7c20a401b052eb0ad
rosell-dk <it@rosell.dk>
2019-10-02 12:44:31 +0200

    removed old code

--- webp-express.php
+++ webp-express.php
@@ -52,55 +52,9 @@ if (Option::getOption('webp-express-alter-html', false)) {
     \WebPExpress\AlterHtmlInit::setHooks();
 }
 
-/*
-$filters = [];
-add_action('all', function() {
-    global $filters;
-    $filter = current_filter();
-    if (!in_array($filter, $filters)) {
-        $filters[] = $filter;
-        error_log($filter);
-    }
-});
-*/
-
 // When images are uploaded with Gutenberg, is_admin() returns false, so, hook needs to be added here
 add_filter('wp_handle_upload', array('\WebPExpress\HandleUploadHooks', 'handleUpload'), 10, 2);
 add_filter('image_make_intermediate_size', array('\WebPExpress\HandleUploadHooks', 'handleMakeIntermediateSize'), 10, 1);
 add_filter('wp_delete_file', array('\WebPExpress\HandleDeleteFileHook', 'deleteAssociatedWebP'), 10, 2);
 
-/*
-add_action('wp_handle_upload', function($a) {
-    error_log('yes, it is called'. (is_admin() ? 'admin' : 'not admin'));
-    \WebPExpress\HandleUploadHooks::handleUpload($a, true);
-    return $a;
-});*/
-
-
-/*
-function webpexpress_addWebPJs() {
-    $url = plugins_url('webpjs/webpjs-0.0.2.min.js', __FILE__);
-    $script = <<<EOD
-<script>
-    (function(){
-        var WebP=new Image();
-        WebP.onload=WebP.onerror=function(){
-            if(WebP.height!=2){
-                var sc=document.createElement('script');
-                sc.type='text/javascript';
-                sc.async=true;
-                var s=document.getElementsByTagName('script')[0];
-                sc.src='$url';
-                s.parentNode.insertBefore(sc,s);
-            }
-        };
-        WebP.src='data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
-    })();
-</script>
-EOD;
-    echo $script;
-}
-
-add_action( 'wp_head', 'webpexpress_addWebPJs');
-*/
 //add_action( 'template_redirect', 'webp_express_template_redirect' );
