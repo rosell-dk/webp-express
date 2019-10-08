3ec6d035a4539f40ea9abc7edfbe7e59cf0a8b71
rosell-dk <it@rosell.dk>
2019-10-08 10:44:44 +0200

    Attempting to fix javascript error in Bulk Convert. #372

--- lib/options/enqueue_scripts.php
+++ lib/options/enqueue_scripts.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 use \WebPExpress\Paths;
 use \WebPExpress\Config;
 
-$ver = '1';             // note: Minimum 1
+$ver = '2';             // note: Minimum 1
 $jsDir = 'js/0.16.0';   // We change dir when it is critical that no-one gets the cached version (there is a plugin that strips version strings out there...)
 
 if (!function_exists('webp_express_add_inline_script')) {
