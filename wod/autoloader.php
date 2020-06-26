e4cbc33aaca569e122c778c6230951dd51aab005
rosell-dk <it@rosell.dk>
2020-06-26 10:16:02 +0200

    minor

--- wod/autoloader.php
+++ wod/autoloader.php
@@ -1,6 +1,9 @@
 <?php
-setlocale(LC_CTYPE, "C.UTF-8");
 define('WOD_DIR', __DIR__);
+
+// setLocale - for converting files with non ascii characters (#406)
+setlocale(LC_CTYPE, "C.UTF-8");
+
 function webpexpress_autoloader($class) {
     if (strpos($class, 'WebPExpress\\') === 0) {
         require_once WOD_DIR . '/../lib/classes/' . substr($class, 12) . '.php';
