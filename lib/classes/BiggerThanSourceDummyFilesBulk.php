71d0eb479171cbe7b6325ab43af4c21e3cf0394c
rosell-dk <bjorn@rosell.dk>
2021-11-02 15:59:18 +0100

    Working on bulk updating dummy files #323

---
+++ lib/classes/BiggerThanSourceDummyFilesBulk.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace WebPExpress;
+
+
+class BiggerThanSourceDummyFilesBulk
+{
+
+    private static $settings;
+
+    /**
+     * Update the status for a all images.
+     *
+     */
+    public static function updateStatus($config)
+    {
+        self::$settings = [
+            'ext' => $config['destination-extension'],
+            'destination-folder' => $config['destination-folder'],  /* hm, "destination-folder" is a bad name... */
+            'webExpressContentDirAbs' => Paths::getWebPExpressContentDirAbs(),
+            'uploadDirAbs' => Paths::getUploadDirAbs(),
+            'useDocRootForStructuringCacheDir' => (($config['destination-structure'] == 'doc-root') && (Paths::canUseDocRootForStructuringCacheDir())),
+            //'imageRoots' => new ImageRoots(Paths::getImageRootsDefForSelectedIds($config['scope'])),   // (Paths::getImageRootsDef()
+            'imageRoots' => new ImageRoots(Paths::getImageRootsDefForSelectedIds(Paths::getImageRootIds())),   // (Paths::getImageRootsDef()
+            'image-types' => $config['image-types'],
+        ];
+
+
+        //$rootIds = Paths::filterOutSubRoots($config['scope']);
+
+        // We want to update status on ALL root dirs (so we don't have to re-run when user changes scope)
+        $rootIds = Paths::filterOutSubRoots(Paths::getImageRootIds());
+        //$rootIds = ['uploads'];
+        //$rootIds = ['uploads', 'themes'];
+
+        foreach ($rootIds as $rootId) {
+            self::updateStatusForRoot($rootId);
+        }
+    }
+
+    /**
+     * Pre-requirement: self::$settings is set.
+     */
+    private static function updateStatusForRoot($rootId, $dir = '')
+    {
+        if ($dir == '') {
+            $dir = Paths::getAbsDirById($rootId);
+        }
+
+        // Canonicalize because dir might contain "/./", which causes file_exists to fail (#222)
+        $dir = PathHelper::canonicalize($dir);
+
+        if (!@file_exists($dir) || !@is_dir($dir)) {
+            return [];
+        }
+
+        $fileIterator = new \FilesystemIterator($dir);
+
+        $results = [];
+
+        while ($fileIterator->valid()) {
+            $filename = $fileIterator->getFilename();
+
+            if (($filename != ".") && ($filename != "..")) {
+                if (@is_dir($dir . "/" . $filename)) {
+                    $newDir = $dir . "/" . $filename;
+
+                    // The new dir might have its own root id
+                    $newRootId = Paths::findImageRootOfPath($newDir, Paths::getImageRootIds());
+                    //echo $newRootId . ': ' . $newDir . "\n";
+                    self::updateStatusForRoot($newRootId, $newDir);
+                } else {
+                    // its a file - check if its a valid image type (jpeg or png)
+                    $regex = '#\.(jpe?g|png)$#';
+                    if (preg_match($regex, $filename)) {
+
+                        $source = $dir . "/" . $filename;
+
+                        $destination = ConvertHelperIndependent::getDestination(
+                            $source,
+                            self::$settings['destination-folder'],
+                            self::$settings['ext'],
+                            self::$settings['webExpressContentDirAbs'],
+                            self::$settings['uploadDirAbs'],
+                            self::$settings['useDocRootForStructuringCacheDir'],
+                            self::$settings['imageRoots'],
+                            //$rootId
+
+                        );
+                        $webpExists = @file_exists($destination);
+
+                        //echo ($webpExists ? 'YES' : 'NO') . ' ' . $rootId . ': ' . $source . "\n";
+
+                        BiggerThanSourceDummyFiles::updateStatus(
+                            $source,
+                            $destination,
+                            self::$settings['webExpressContentDirAbs'],
+                            self::$settings['imageRoots'],
+                            self::$settings['destination-folder'],
+                            self::$settings['ext'],
+                            // $rootId,
+                        );
+
+                    }
+                }
+            }
+            $fileIterator->next();
+        }
+        return $results;
+    }
+}
